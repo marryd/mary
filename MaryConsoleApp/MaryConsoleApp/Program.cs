33f529304eabddeea763c31d6d78d7c86aa634e5
marryd <markus.rydin@kentor.se>
2014-09-15 22:34:20 +0200

    app menu added

--- MaryConsoleApp/MaryConsoleApp/Program.cs
+++ MaryConsoleApp/MaryConsoleApp/Program.cs
@@ -10,6 +10,27 @@ namespace MaryConsoleApp
     {
         static void Main(string[] args)
         {
+            showStartMenu();
+
+            var function = new ConsoleKeyInfo();
+
+            do
+            {
+                function = Console.ReadKey();
+
+                Console.WriteLine(string.Format("\nYou pressed '{0}' button.", function.KeyChar));
+
+            }while(function.Key != ConsoleKey.Escape);
+        }
+
+        private static void showStartMenu()
+        {
+            Console.WriteLine("*********** Welcome Menu ***********");
+            Console.WriteLine("\tA\t\t - Add");
+            Console.WriteLine("\tD\t\t - Delete");
+            Console.WriteLine("\tEsc\t\t - Quit");
+            Console.WriteLine();
+
         }
     }
 }
